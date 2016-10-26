ba095d7747b299774ee93ea3ddc53cb315df238f
BalazsToth <t.balazs1986@gmail.com>
2016-10-26 12:15:40 +0200

    Change find_word function content

--- src/Common.cpp
+++ src/Common.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// Name:        Common.pp
+// Name:        Common.cpp
 // Purpose:     Common namespace
 // Author:      Balázs Tóth
 // Modified by:
@@ -7,9 +7,9 @@
 // Copyright:   (c) Common utils
 // Licence:     GPL
 /////////////////////////////////////////////////////////////////////////////
+
 #include "Common.h"
 
-<<<<<<< HEAD
 namespace Common {
 	std::string to_string(float const& number) {
 		std::ostringstream buffer;
@@ -22,36 +22,28 @@ namespace Common {
 	    return std::string{buffer.str()}; 
 	}
 	std::vector<int> find_word(std::string const& text, std::string const& word) {
-		std::vector<int> positions;
-		int pos = 0;
-		while(pos<text.size()-word.size()) {
-			pos = text.find(word, pos);
-			positions.push_back(pos);
-			if(pos==-1) break;
-			pos++;
+		// std::vector<int> positions;
+		// int pos = 0;
+		// while(pos<text.size()-word.size()) {
+		// 	pos = text.find(word, pos);
+		// 	positions.push_back(pos);
+		// 	if(pos==-1) break;
+		// 	pos++;
+		// }
+		// return positions;
+		std::vector<int> start;
+		for(int i=0; i<text.size(); i++) {
+			bool match = true;
+			for(int j=0; j<word.size(); j++) {
+				if(text[i+j]!=word[j] || i+j>=text.size()) {
+					match = false;
+					break;
+				}
+			}
+			if(match){
+				start.push_back(i);
+			}
 		}
-		return positions;
-=======
-using namespace Common;
-
-std::string to_string(float const& number) {
-    std::ostringstream buffer;
-    buffer << number;
-    return std::string{buffer.str()}; 
-}
-std::string to_string(size_t const& number) {
-    std::ostringstream buffer;
-    buffer << number;
-    return std::string{buffer.str()}; 
-}
-std::vector<int> find_word(std::string const& text, std::string const& word) {
-	std::vector<int> positions;
-	int pos = 0;
-	while(pos<text.size()-word.size()) {
-		pos = text.find(word, pos);
-		positions.push_back(pos);
-		if(pos==-1) break;
-		pos++;
->>>>>>> 3306d5a0b6a7249cc6ece50009b2bd0289f1598c
+		return start;
 	}
 }
