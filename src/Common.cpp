3306d5a0b6a7249cc6ece50009b2bd0289f1598c
Balázs Tóth <t.balazs1986@gmail.com>
2016-10-26 09:09:11 +0000

    Common.cpp edited online

--- src/Common.cpp
+++ src/Common.cpp
@@ -1,14 +1,23 @@
+/////////////////////////////////////////////////////////////////////////////
+// Name:        Common.pp
+// Purpose:     Common namespace
+// Author:      Balázs Tóth
+// Modified by:
+// Created:     10/07/2016
+// Copyright:   (c) Common utils
+// Licence:     GPL
+/////////////////////////////////////////////////////////////////////////////
 #include "Common.h"
 
 using namespace Common;
 
 std::string to_string(float const& number) {
-	std::ostringstream buffer;
+    std::ostringstream buffer;
     buffer << number;
     return std::string{buffer.str()}; 
 }
 std::string to_string(size_t const& number) {
-	std::ostringstream buffer;
+    std::ostringstream buffer;
     buffer << number;
     return std::string{buffer.str()}; 
 }
